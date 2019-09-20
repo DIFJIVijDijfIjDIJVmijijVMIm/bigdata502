7fe3f344dad4e3f96c67161db77146a41e4173b5
kim <kim@naver.com>
2019-09-20 12:18:00 +0900

    test 추가

--- Animal.java
+++ Animal.java
@@ -8,10 +8,14 @@ public class Animal {
 	public void play2() {
 		System.out.println("¾ß¿Ë");
 	}
+	public void test() {
+		System.out.println("¾È³ç");
+	}
 	
 	public static void main(String[] args) {
 		Animal a = new Animal();
 		a.play1();
 		a.play2();
+		a.test();
 	}
 }
